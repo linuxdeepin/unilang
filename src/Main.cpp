09e9d90b3fefafcf52a37e25e26b597ab9e576e6
He Bin <hebin@uniontech.com>
2020-07-10 17:01:55 +0800

    src/Main.cpp: Added term node class.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,14 +1,17 @@
 ﻿// © 2020-2020 Uniontech Software Technology Co.,Ltd.
 
 #include <memory_resource> // for complete std::pmr::polymorphic_allocator;
+#include <list> // for std::pmr::list;
 #include <string> // for std::pmr::string, std::getline;
 #include <string_view> // for std::string_view;
+#include <any> // for std::any;
 #include <iostream>
 
 namespace Unilang
 {
 
-using string = std::pmr::string;
+using std::pmr::list;
+using std::pmr::string;
 using std::string_view;
 
 } // namespace Unilang;
@@ -17,6 +20,76 @@ namespace
 {
 
 using namespace Unilang;
+
+using ValueObject = std::any;
+
+
+class TermNode final
+{
+public:
+	using Container = list<TermNode>;
+	using allocator_type = Container::allocator_type;
+	using iterator = Container::iterator;
+	using const_iterator = Container::const_iterator;
+	using reverse_iterator = Container::reverse_iterator;
+	using const_reverse_iterator = Container::const_reverse_iterator;
+
+	Container Subterms{};
+	ValueObject Value{};
+
+	TermNode() = default;
+	explicit
+	TermNode(allocator_type a)
+		: Subterms(a)
+	{}
+	TermNode(const Container& con)
+		: Subterms(con)
+	{}
+	TermNode(Container&& con)
+		: Subterms(std::move(con))
+	{}
+	TermNode(const TermNode&) = default;
+	TermNode(const TermNode& tm, allocator_type a)
+		: Subterms(tm.Subterms, a), Value(tm.Value)
+	{}
+	TermNode(TermNode&&) = default;
+	TermNode(TermNode&& tm, allocator_type a)
+		: Subterms(std::move(tm.Subterms), a), Value(std::move(tm.Value))
+	{}
+	
+	TermNode&
+	operator=(TermNode&&) = default;
+
+	void
+	Add(const TermNode& term)
+	{
+		Subterms.push_back(term);
+	}
+	void
+	Add(TermNode&& term)
+	{
+		Subterms.push_back(std::move(term));
+	}
+
+	[[nodiscard, gnu::pure]]
+	allocator_type
+	get_allocator() const noexcept
+	{
+		return Subterms.get_allocator();
+	}
+};
+
+
+template<typename... _tParams>
+[[nodiscard, gnu::pure]] inline TermNode
+AsTermNode(_tParams&&... args)
+{
+	TermNode term;
+
+	term.Value = ValueObject(std::forward<_tParams>(args)...);
+	return term;
+}
+
 
 class Interpreter final
 {
@@ -68,7 +141,7 @@ Interpreter::WaitForLine()
 
 
 #define APP_NAME "Unilang demo"
-#define APP_VER "0.0.1"
+#define APP_VER "0.0.2"
 #define APP_PLATFORM "[C++17]"
 constexpr auto
 	title(APP_NAME " " APP_VER " @ (" __DATE__ ", " __TIME__ ") " APP_PLATFORM);
