7ce66f961c938ae3e4c4031eb705818879cc04c8
He Bin <hebin@uniontech.com>
2020-07-31 22:09:45 +0800

    build.sh: Updated source list.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ g++ -std=c++14 -Wall -Wextra -g -ounilang src/*.cpp \
 	3rdparty/YSLib/YBase/source/ystdex/any.cpp \
 	3rdparty/YSLib/YBase/source/ystdex/cstdio.cpp \
 	3rdparty/YSLib/YBase/source/ystdex/cassert.cpp \
+	3rdparty/YSLib/YBase/source/ystdex/exception.cpp \
 	3rdparty/YSLib/YBase/source/ystdex/memory_resource.cpp \
 	3rdparty/YSLib/YBase/source/ystdex/node_base.cpp
 
