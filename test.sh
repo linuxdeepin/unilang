e607e9ae1bd74f0bf04f5044ede2bf1a7f2a7c43
He Bin <hebin@uniontech.com>
2020-07-31 16:34:46 +0800

    Added test script with current supported cases.
    Signed-off-by: He Bin <hebin@uniontech.com>

---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+set -e
+
+OUT=/tmp/out
+ERR=/tmp/err
+
+touch "$OUT"
+touch "$ERR"
+
+run()
+{
+	echo > "$ERR"
+	echo "$1" | ./unilang 1> "$OUT" 2> "$ERR"
+}
+
+# NOTE: Test cases should print no errors.
+test()
+{
+	echo "Running case:" "$1"
+	run "$1"
+	if [ -s "$ERR" ]; then
+		echo "FAIL."
+		echo "Error:"
+		cat "$ERR"
+	else
+		echo "PASS."
+	fi
+}
+
+# Sanity.
+test 'display'
+
+# Documented examples.
+test 'display "Hello, world!"'
+test 'display (display "Hello, world!")'
+test '(display "Hello, world!")'
+test '() newline'
+test '() display "Hello, world!"'
+test 'display "Hello, world!";'
+test '$sequence display "Hello, world!"'
+test 'display "Hello, "; display "world!"'
+test '$sequence (display "Hello, ") (display "world!")'
+
