4abb0fe51f20361cde20eddcbe895db27005a4c7
He Bin <hebin@uniontech.com>
2020-08-28 19:19:21 +0800

    Retargeted the build environment to ISO C++ 11.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
-g++ -std=c++14 -Wall -Wextra -g -ounilang src/*.cpp \
+g++ -std=c++11 -Wall -Wextra -g -ounilang src/*.cpp \
 	-I3rdparty/YSLib/YBase/include \
 	3rdparty/YSLib/YBase/source/ystdex/any.cpp \
 	3rdparty/YSLib/YBase/source/ystdex/cstdio.cpp \
 	3rdparty/YSLib/YBase/source/ystdex/cassert.cpp \
 	3rdparty/YSLib/YBase/source/ystdex/exception.cpp \
 	3rdparty/YSLib/YBase/source/ystdex/memory_resource.cpp \
-	3rdparty/YSLib/YBase/source/ystdex/node_base.cpp
+	3rdparty/YSLib/YBase/source/ystdex/node_base.cpp \
+	3rdparty/YSLib/YBase/source/ystdex/tree.cpp
 
