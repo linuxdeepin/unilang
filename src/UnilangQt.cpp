9209d5a2d3b663fd572bc14df5b6a15787ec78c0
He Bin <hebin@uniontech.com>
2020-12-11 20:53:53 +0800

    Added function: QWidget-resize.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/UnilangQt.cpp
+++ src/UnilangQt.cpp
@@ -7,17 +7,29 @@
 #	pragma GCC diagnostic ignored "-Wsign-conversion"
 #	pragma GCC diagnostic ignored "-Wsign-promo"
 #endif
-#include <QApplication> // for QApplication;
 #include <QWidget> // for QWidget;
+#include <QApplication> // for QApplication;
 #if __GNUC__
 #	pragma GCC diagnostic pop
 #endif
+#include <cassert> // for assert;
 
 namespace Unilang
 {
 
 namespace
 {
+
+using YSLib::unique_ptr;
+
+QWidget&
+ResolveQWidget(TermNode& term)
+{
+	const auto& p_wgt(Unilang::ResolveRegular<unique_ptr<QWidget>>(term));
+
+	assert(p_wgt.get());
+	return *p_wgt;
+}
 
 } // unnamed namespace;
 
@@ -27,7 +39,6 @@ InitializeQt(Interpreter& intp, int& argc, char* argv[])
 {
 	auto& ctx(intp.Root);
 	using namespace Forms;
-	using YSLib::unique_ptr;
 	using YSLib::make_unique;
 
 	RegisterStrict(ctx, "make-QApplication", [&, argv](TermNode& term){
@@ -45,9 +56,19 @@ InitializeQt(Interpreter& intp, int& argc, char* argv[])
 		term.Value = make_unique<QWidget>();
 		return ReductionStatus::Clean;
 	});
+	RegisterStrict(ctx, "QWidget-resize", [](TermNode& term){
+		RetainN(term, 3);
+
+		auto i(term.begin());
+		auto& wgt(ResolveQWidget(*++i));
+		const auto& w(Unilang::ResolveRegular<int>(*++i));
+		const auto& h(Unilang::ResolveRegular<int>(*++i));
+
+		wgt.resize(w, h);
+		return ReduceReturnUnspecified(term);
+	});
 	RegisterUnary<Strict, const unique_ptr<QWidget>>(ctx, "QWidget-show",
 		[](const unique_ptr<QWidget>& p_wgt){
-		p_wgt->resize(800, 600);
 		p_wgt->show();
 		return ValueToken::Unspecified;
 	});
