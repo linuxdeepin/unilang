c64cf60abce240c27ffebb55e0d11c096f94d596
He Bin <hebin@uniontech.com>
2020-12-12 13:04:46 +0800

    src/UnilangQt.cpp: Added native API for the Qt demo application.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/UnilangQt.cpp
+++ src/UnilangQt.cpp
@@ -7,8 +7,11 @@
 #	pragma GCC diagnostic ignored "-Wsign-conversion"
 #	pragma GCC diagnostic ignored "-Wsign-promo"
 #endif
-#include <QWidget> // for QWidget;
 #include <QApplication> // for QApplication;
+#include <QWidget> // for QWidget;
+#include <QPushButton> // for QPushButton;
+#include <QLabel> // for QString, QLabel;
+#include <QBoxLayout> // for QLayout, QVBoxLayout;
 #if __GNUC__
 #	pragma GCC diagnostic pop
 #endif
@@ -71,6 +74,53 @@ InitializeQt(Interpreter& intp, int& argc, char* argv[])
 		[](const shared_ptr<QWidget>& p_wgt){
 		p_wgt->show();
 		return ValueToken::Unspecified;
+	});
+	RegisterStrict(ctx, "QWidget-setLayout", [](TermNode& term){
+		RetainN(term, 2);
+
+		auto i(term.begin());
+		auto& wgt(*Unilang::ResolveRegular<shared_ptr<QWidget>>(*++i));
+		auto& layout(*Unilang::ResolveRegular<shared_ptr<QLayout>>(*++i));
+
+		wgt.setLayout(&layout);
+		return ReduceReturnUnspecified(term);
+	});
+	RegisterStrict(ctx, "make-QPushButton", [](TermNode& term){
+		RetainN(term, 1);
+
+		auto i(term.begin());
+
+		term.Value = shared_ptr<QWidget>(make_shared<QPushButton>(
+			Unilang::ResolveRegular<string>(*++i).c_str()));
+		return ReductionStatus::Clean;
+	});
+	ctx.GetRecordRef().Bindings["Qt.AlignCenter"].Value = Qt::AlignCenter;
+	RegisterStrict(ctx, "make-QLabel", [](TermNode& term){
+		RetainN(term, 2);
+
+		auto i(term.begin());
+		const auto& text(Unilang::ResolveRegular<string>(*++i));
+		const auto& align(Unilang::ResolveRegular<Qt::AlignmentFlag>(*++i));
+		auto p_lbl(make_shared<QLabel>(QString(text.c_str())));
+
+		p_lbl->setAlignment(align);
+		term.Value = shared_ptr<QWidget>(std::move(p_lbl));
+		return ReductionStatus::Clean;
+	});
+	RegisterStrict(ctx, "make-QVBoxLayout", [](TermNode& term){
+		RetainN(term, 0);
+		term.Value = shared_ptr<QLayout>(make_shared<QVBoxLayout>());
+		return ReductionStatus::Clean;
+	});
+	RegisterStrict(ctx, "QLayout-addWidget", [](TermNode& term){
+		RetainN(term, 2);
+
+		auto i(term.begin());
+		auto& layout(*Unilang::ResolveRegular<shared_ptr<QLayout>>(*++i));
+		auto& wgt(*Unilang::ResolveRegular<shared_ptr<QWidget>>(*++i));
+
+		layout.addWidget(&wgt);
+		return ReduceReturnUnspecified(term);
 	});
 }
 
