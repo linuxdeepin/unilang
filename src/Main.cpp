a1cad41b1cbeed38fd5a813572bd576d6368db52
He Bin <hebin@uniontech.com>
2020-12-17 19:05:10 +0800

    src/Main.cpp: Used sigil '&' in all 27 parameter trees in function derivations.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/Main.cpp
+++ src/Main.cpp
@@ -47,17 +47,17 @@ LoadFunctions(Interpreter& intp)
 	RegisterStrict(ctx, "make-encapsulation-type", MakeEncapsulationType);
 	RegisterStrict(ctx, "get-current-environment", GetCurrentEnvironment);
 	intp.Perform(R"Unilang(
-		$def! $vau $vau/e (() get-current-environment) (formals ef .body) d
+		$def! $vau $vau/e (() get-current-environment) (&formals &ef .&body) d
 			eval (cons $vau/e (cons d (cons formals (cons ef body)))) d;
 		$def! lock-current-environment (wrap ($vau () d lock-environment d));
-		$def! $lambda $vau (formals .body) d wrap
+		$def! $lambda $vau (&formals .&body) d wrap
 			(eval (cons $vau (cons formals (cons ignore body))) d);
 	)Unilang");
 	RegisterStrict(ctx, "list", ReduceBranchToListValue);
 	intp.Perform(R"Unilang(
-		$def! $set! $vau (e formals .expr) d
+		$def! $set! $vau (&e &formals .&expr) d
 			eval (list $def! formals (unwrap eval) expr d) (eval e d);
-		$def! $defv! $vau ($f formals ef .body) d
+		$def! $defv! $vau (&$f &formals &ef .&body) d
 			eval (list $set! d $f $vau formals ef body) d;
 		$defv! $defl! (f formals .body) d
 			eval (list $set! d f $lambda formals body) d;
@@ -68,27 +68,27 @@ LoadFunctions(Interpreter& intp)
 	)Unilang");
 	RegisterForm(ctx, "$sequence", Sequence);
 	intp.Perform(R"Unilang(
-		$defl! first ((x .)) x;
+		$defl! first ((&x .)) x;
 		$defl! rest ((#ignore .x)) x;
-		$defl! apply (appv arg .opt)
+		$defl! apply (&appv &arg .&opt)
 			eval (cons () (cons (unwrap appv) arg))
 				($if (null? opt) (() make-environment)
-					(($lambda ((e .eopt))
+					(($lambda ((&e .&eopt))
 						$if (null? eopt) e
 							(raise-invalid-syntax-error
 								"Syntax error in applying form.")) opt));
-		$defl! list* (head .tail)
+		$defl! list* (&head .&tail)
 			$if (null? tail) head (cons head (apply list* tail));
-		$defv! $defw! (f formals ef .body) d
+		$defv! $defw! (&f &formals &ef .&body) d
 			eval (list $set! d f wrap (list* $vau formals ef body)) d;
 		$defv! $cond clauses d
 			$if (null? clauses) #inert
-				(apply ($lambda ((test .body) .clauses)
+				(apply ($lambda ((&test .&body) .&clauses)
 					$if (eval test d) (eval body d)
 						(apply (wrap $cond) clauses d)) clauses);
-		$defv! $when (test .exprseq) d
+		$defv! $when (&test .&exprseq) d
 			$if (eval test d) (eval (list* () $sequence exprseq) d);
-		$defv! $unless (test .exprseq) d
+		$defv! $unless (&test .&exprseq) d
 			$if (eval test d) #inert (eval (list* () $sequence exprseq) d);
 		$defl! not? (x) eqv? x #f;
 		$defv! $and? x d $cond
@@ -101,50 +101,50 @@ LoadFunctions(Interpreter& intp)
 			((null? (rest x)) eval (first x) d)
 			(#t ($lambda (r) $if r r
 				(apply (wrap $or?) (rest x) d)) (eval (first x) d));
-		$defw! accr (l pred? base head tail sum) d
+		$defw! accr (&l &pred? &base &head &tail &sum) d
 			$if (apply pred? (list l) d) base
 				(apply sum (list (apply head (list l) d)
 					(apply accr (list (apply tail (list l) d)
 					pred? base head tail sum) d)) d);
-		$defw! foldr1 (kons knil l) d
+		$defw! foldr1 (&kons &knil &l) d
 			apply accr (list l null? knil first rest kons) d;
-		$defw! map1 (appv l) d
-			foldr1 ($lambda (x xs) cons (apply appv (list x) d) xs) () l;
-		$defl! list-concat (x y) foldr1 cons y x;
-		$defl! append (.ls) foldr1 list-concat () ls;
-		$defv! $let (bindings .body) d
+		$defw! map1 (&appv &l) d
+			foldr1 ($lambda (&x &xs) cons (apply appv (list x) d) xs) () l;
+		$defl! list-concat (&x &y) foldr1 cons y x;
+		$defl! append (.&ls) foldr1 list-concat () ls;
+		$defv! $let (&bindings .&body) d
 			eval (list* () (list* $lambda (map1 first bindings)
 				(list body)) (map1 ($lambda (x) list (rest x)) bindings)) d;
-		$defv! $let/d (bindings ef .body) d
+		$defv! $let/d (&bindings &ef .&body) d
 			eval (list* () (list wrap (list* $vau (map1 first bindings)
 				ef (list body))) (map1 ($lambda (x) list (rest x)) bindings)) d;
-		$defv! $let* (bindings .body) d
+		$defv! $let* (&bindings .&body) d
 			eval ($if (null? bindings) (list* $let bindings body)
 				(list $let (list (first bindings))
 				(list* $let* (rest bindings) body))) d;
-		$defv! $letrec (bindings .body) d
+		$defv! $letrec (&bindings .&body) d
 			eval (list $let () $sequence (list $def! (map1 first bindings)
 				(list* () list (map1 rest bindings))) body) d;
-		$defv! $bindings/p->environment (parents .bindings) d $sequence
+		$defv! $bindings/p->environment (&parents .&bindings) d $sequence
 			($def! res apply make-environment (map1 ($lambda (x) eval x d)
 				parents))
 			(eval (list $set! res (map1 first bindings)
 				(list* () list (map1 rest bindings))) d)
 			res;
-		$defv! $bindings->environment (.bindings) d
+		$defv! $bindings->environment (.&bindings) d
 			eval (list* $bindings/p->environment
 				(list (() make-standard-environment)) bindings) d;
-		$defv! $provide! (symbols .body) d
+		$defv! $provide! (&symbols .&body) d
 			$sequence (eval (list $def! symbols (list $let () $sequence
 				(list ($vau (e) d $set! e res (lock-environment d))
 				(() get-current-environment)) body
 				(list* () list symbols))) d) res;
-		$defv! $provide/d! (symbols ef .body) d
+		$defv! $provide/d! (&symbols &ef .body) d
 			$sequence (eval (list $def! symbols (list $let/d () ef $sequence
 				(list ($vau (e) d $set! e res (lock-environment d))
 				(() get-current-environment)) body
 				(list* () list symbols))) d) res;
-		$defv! $import! (e .symbols) d
+		$defv! $import! (&e .&symbols) d
 			eval (list $set! d symbols (list* () list symbols)) (eval e d);
 	)Unilang");
 	RegisterStrict(ctx, "random.choice", [&](TermNode& term){
@@ -195,7 +195,7 @@ LoadFunctions(Interpreter& intp)
 }
 
 #define APP_NAME "Unilang demo"
-#define APP_VER "0.5.17"
+#define APP_VER "0.5.18"
 #define APP_PLATFORM "[C++11] + YSLib"
 constexpr auto
 	title(APP_NAME " " APP_VER " @ (" __DATE__ ", " __TIME__ ") " APP_PLATFORM);
