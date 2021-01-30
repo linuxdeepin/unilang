a5021591f3b46d045a3d3b90126a1b948669dee1
He Bin <hebin@uniontech.com>
2021-01-30 12:22:40 +0800

    Added stub for arithmetic operations.
    Signed-off-by: He Bin <hebin@uniontech.com>

---
+++ src/Arithmetic.cpp
@@ -0,0 +1,14 @@
+﻿// © 2021 Uniontech Software Technology Co.,Ltd.
+
+#include "Arithmetic.h"
+
+namespace Unilang
+{
+
+inline namespace Numbers
+{
+
+} // inline namespace Numbers;
+
+} // namespace Unilang;
+
