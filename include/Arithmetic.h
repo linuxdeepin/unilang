77216f071d0263d8277be40a68ea1a1d32b9f168
He Bin <hebin@uniontech.com>
2021-02-06 14:45:44 +0800

    {include, src}/Arithmetic: Replaced int to class Number with ordered field operators.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- include/Arithmetic.h
+++ include/Arithmetic.h
@@ -4,6 +4,7 @@
 #define INC_Unilang_Arithmetic_h_ 1
 
 #include "Unilang.h"
+#include <ystdex/operators.hpp> // for ystdex::ordered_field_operators;
 
 namespace Unilang
 {
@@ -11,7 +12,33 @@ namespace Unilang
 inline namespace Numbers
 {
 
-using Number = int;
+class Number : private ystdex::ordered_field_operators<Number>
+{
+private:
+	int value;
+
+public:
+	Number(int);
+
+	Number&
+	operator+=(const Number&);
+
+	Number&
+	operator-=(const Number&);
+
+	Number&
+	operator*=(const Number&);
+
+	[[nodiscard, gnu::pure]] friend bool
+	operator==(const Number& x, const Number& y) noexcept;
+
+	[[nodiscard, gnu::pure]] friend bool
+	operator<(const Number& x, const Number& y) noexcept;
+
+	// XXX: Temporary workaround to avoid ambiguity
+	[[nodiscard, gnu::pure]] friend Number
+	operator-(const Number& x, const Number& y);
+};
 
 } // inline namespace Numbers;
 
