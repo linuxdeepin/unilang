0dc87ddebcdd5882d87b7403cb6ad22dbece265d
He Bin <hebin@uniontech.com>
2021-02-27 13:51:45 +0800

    include, src: Added TCO stubs.
    Signed-off-by: He Bin <hebin@uniontech.com>

---
+++ include/TCO.h
@@ -0,0 +1,14 @@
+﻿// © 2021 Uniontech Software Technology Co.,Ltd.
+
+#ifndef INC_Unilang_TCO_h_
+#define INC_Unilang_TCO_h_ 1
+
+#include "Context.h"
+
+namespace Unilang
+{
+
+} // namespace Unilang;
+
+#endif
+
---
+++ src/TCO.cpp
@@ -0,0 +1,9 @@
+﻿// © 2021 Uniontech Software Technology Co.,Ltd.
+
+#include "TCO.h"
+
+namespace Unilang
+{
+
+} // namespace Unilang;
+
