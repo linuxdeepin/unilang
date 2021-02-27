21ff60ab08cbf817930a888b7a90e0ac5f454329
He Bin <hebin@uniontech.com>
2021-02-27 13:50:56 +0800

    sbuild.sh: Worked around MSYS2 MinGW64 linking error due to ASLR enabled in the toolchain.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- sbuild.sh
+++ sbuild.sh
@@ -10,6 +10,19 @@ Unilang_BaseDir="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
 	"ERROR: The configuration name should not be empty." >& 2; exit 1)
 
 case $(uname) in
+*MINGW64*)
+# XXX: Workaround for MinGW64 G++ + ld with ASLR enabled by default. Clang++
+#	+ ld or G++ + lld do need the flag, but leaving it here as-is.
+#	See https://www.msys2.org/news/#2021-01-31-aslr-enabled-by-default,
+#	https://github.com/msys2/MINGW-packages/issues/6986,
+#	https://github.com/msys2/MINGW-packages/issues/7023,
+#	and https://sourceware.org/bugzilla/show_bug.cgi?id=26659.
+	LDFLAGS_LOWBASE_=-Wl,--default-image-base-low
+	;;
+*)
+	SHBuild_LIBS='-lffi -ldl'
+esac
+case $(uname) in
 *MSYS* | *MINGW*)
 	SHBuild_LIBS=-lffi
 	;;
@@ -18,9 +31,9 @@ case $(uname) in
 esac
 mkdir -p "$Unilang_BaseDir/build"
 (cd "$Unilang_BaseDir/build" \
-	&& SHBuild_NoAdjustSubsystem=true SHBuild_LIBS="$SHBuild_LIBS" \
-	SHBuild-BuildPkg.sh "$@" -xn,unilang "$Unilang_BaseDir/src" \
-	-I\""$Unilang_BaseDir/include\"")
+	&& SHBuild_NoAdjustSubsystem=true SHBuild_LDFLAGS="$LDFLAGS_LOWBASE_" \
+	SHBuild_LIBS="$SHBuild_LIBS" SHBuild-BuildPkg.sh "$@" \
+	-xn,unilang "$Unilang_BaseDir/src" -I\""$Unilang_BaseDir/include\"")
 
 echo "Done."
 
