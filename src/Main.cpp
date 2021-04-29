4193ae38335c1bfd3fd4aef2d8b6b090dc911c60
He Bin <hebin@uniontech.com>
2021-04-29 23:17:19 +0800

    src/Main.cpp: Updated the version number.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/Main.cpp
+++ src/Main.cpp
@@ -502,7 +502,7 @@ LoadFunctions(Interpreter& intp)
 }
 
 #define APP_NAME "Unilang demo"
-#define APP_VER "0.6.125"
+#define APP_VER "0.6.129"
 #define APP_PLATFORM "[C++11] + YSLib"
 constexpr auto
 	title(APP_NAME " " APP_VER " @ (" __DATE__ ", " __TIME__ ") " APP_PLATFORM);
