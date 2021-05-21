2bef86933ae331b4375fc9650cbb2ebaafbb51d7
He Bin <hebin@uniontech.com>
2021-05-21 11:17:28 +0800

    src/LLVM.cpp: Added driver stub.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/LLVM.cpp
+++ src/LLVM.cpp
@@ -1,12 +1,7 @@
 ﻿// © 2021 Uniontech Software Technology Co.,Ltd.
 
-// XXX: No other headers are included to allow coexistence of different version
-//	of the standard libraries.
+#include "Unilang.h"
 #if __GNUG__
-#	pragma push_macro("_GLIBCXX_DEBUG")
-#	pragma push_macro("_GLIBCXX_DEBUG_PEDANTIC")
-#	undef _GLIBCXX_DEBUG
-#	undef _GLIBCXX_DEBUG_PEDANTIC
 #	pragma GCC diagnostic push
 #	pragma GCC diagnostic ignored "-Wctor-dtor-privacy"
 #	pragma GCC diagnostic ignored "-Wshadow"
@@ -14,10 +9,11 @@
 #	pragma GCC diagnostic ignored "-Wunused-parameter"
 #endif
 #include "llvm/ADT/STLExtras.h"
+#include "llvm/Support/TargetSelect.h" // for llvm::InitializeNativeTarget,
+//	 llvm::InitializeNativeTargetAsmPrinter,
+//	llvm::InitializeNativeTargetAsmParser;
 #if __GNUG__
 #	pragma GCC diagnostic pop
-#	pragma pop_macro("_GLIBCXX_DEBUG_PEDANTIC")
-#	pragma pop_macro("_GLIBCXX_DEBUG")
 #endif
 
 namespace Unilang
@@ -26,9 +22,48 @@ namespace Unilang
 void
 llvm_main();
 
+namespace
+{
+
+class Driver final
+{
+public:
+	Driver();
+
+	void
+	InitializeModuleAndPassManager();
+
+	void
+	Run();
+};
+
+Driver::Driver()
+{
+	llvm::InitializeNativeTarget();
+	llvm::InitializeNativeTargetAsmPrinter();
+	llvm::InitializeNativeTargetAsmParser();
+	InitializeModuleAndPassManager();
+}
+
+void
+Driver::InitializeModuleAndPassManager()
+{
+}
+
+void
+Driver::Run()
+{
+}
+
+} // unnamed namespace;
+
 void
 llvm_main()
-{}
+{
+	Driver d;
+
+	d.Run();
+}
 
 } // namespace Unilang;
 
