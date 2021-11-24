ef8a98c0dff0ec9ba676e671c42248129a6cb5fe
He Bin <hebin@uniontech.com>
2021-11-25 02:02:07 +0800

    {include, src}/Parser: Added a base class of the parser and an assertion message.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- include/Parser.h
+++ include/Parser.h
@@ -3,23 +3,56 @@
 #ifndef INC_Unilang_Parser_h_
 #define INC_Unilang_Parser_h_ 1
 
-#include "Unilang.h" // for vector, string;
+#include "Lexical.h" // for lref, LexicalAnalyzer, string, pmr, vector;
 
 namespace Unilang
 {
 
-class ByteParser final
+class BufferedByteParserBase
+{
+public:
+	char Delimiter = {};
+
+private:
+	string buffer{};
+
+public:
+	BufferedByteParserBase(pmr::polymorphic_allocator<byte> a = {})
+		: buffer(a)
+	{}
+
+	YB_ATTR_nodiscard YB_PURE const string&
+	GetBuffer() const noexcept
+	{
+		return buffer;
+	}
+	YB_ATTR_nodiscard YB_PURE string&
+	GetBufferRef() noexcept
+	{
+		return buffer;
+	}
+	YB_ATTR_nodiscard YB_PURE char&
+	GetBackRef() noexcept
+	{
+		return buffer.back();
+	}
+};
+
+
+class ByteParser final : BufferedByteParserBase
 {
 public:
 	using ParseResult = vector<string>;
 
 private:
-	char Delimiter = {};
-	string buffer{};
 	mutable ParseResult lexemes{};
 	bool update_current = {};
 
 public:
+	ByteParser(pmr::polymorphic_allocator<yimpl(byte)> a = {})
+		: BufferedByteParserBase(a), lexemes(a)
+	{}
+
 	void
 	operator()(char);
 
