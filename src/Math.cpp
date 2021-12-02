da5477881799622ad053eec190ed327b21237b87
He Bin <hebin@uniontech.com>
2021-12-02 13:25:07 +0800

    {include, src}/Math: Added number subtype value predicates.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/Math.cpp
+++ src/Math.cpp
@@ -1,12 +1,32 @@
 ﻿// © 2021 Uniontech Software Technology Co.,Ltd.
 
-#include "Math.h"
+#include "Math.h" // for Unilang::TryAccessValue;
+#include <cmath> // for std::isfinite;
 
 namespace Unilang
 {
 
 inline namespace Math
 {
+
+namespace
+{
+
+template<typename _type>
+YB_ATTR_nodiscard YB_PURE bool
+FloatIsInteger(const _type& x) noexcept
+{
+#if __GNUC__
+#	pragma GCC diagnostic push
+#	pragma GCC diagnostic ignored "-Wfloat-equal"
+#endif
+	return std::isfinite(x) && std::nearbyint(x) == x;
+#if __GNUC__
+#	pragma GCC diagnostic pop
+#endif
+}
+
+} // unnamed namespace;
 
 bool
 IsExactValue(const ValueObject& vo) noexcept
@@ -23,6 +43,30 @@ IsInexactValue(const ValueObject& vo) noexcept
 {
 	return
 		IsTyped<double>(vo) || IsTyped<float>(vo) || IsTyped<long double>(vo);
+}
+
+bool
+IsRationalValue(const ValueObject& vo) noexcept
+{
+	if(const auto p_d = Unilang::TryAccessValue<double>(vo))
+		return std::isfinite(*p_d);
+	if(const auto p_f = Unilang::TryAccessValue<float>(vo))
+		return std::isfinite(*p_f);
+	if(const auto p_ld = Unilang::TryAccessValue<long double>(vo))
+		return std::isfinite(*p_ld);
+	return IsExactValue(vo);
+}
+
+bool
+IsIntegerValue(const ValueObject& vo) noexcept
+{
+	if(const auto p_d = Unilang::TryAccessValue<double>(vo))
+		return FloatIsInteger(*p_d);
+	if(const auto p_f = Unilang::TryAccessValue<float>(vo))
+		return FloatIsInteger(*p_f);
+	if(const auto p_ld = Unilang::TryAccessValue<long double>(vo))
+		return FloatIsInteger(*p_ld);
+	return IsExactValue(vo);
 }
 
 
