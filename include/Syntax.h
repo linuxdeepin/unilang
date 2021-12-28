85d4b5b26287fd8fb7cdfc992e6a859bb152adfd
He Bin <hebin@uniontech.com>
2021-12-28 17:11:47 +0800

    Implemented parsing of extended boundary tokens.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- include/Syntax.h
+++ include/Syntax.h
@@ -6,6 +6,7 @@
 #include "TermNode.h" // for TermNode, stack;
 #include "Exception.h" // for UnilangException;
 #include <cassert> // for assert;
+#include "Lexical.h" // for ThrowMismatchBoundaryToken;
 
 namespace Unilang
 {
@@ -23,6 +24,7 @@ struct LexemeTokenizer final
 		return AsTermNode(val);
 	}
 };
+
 
 template<typename _tIn>
 YB_ATTR_nodiscard inline _tIn
@@ -51,9 +53,12 @@ ReduceSyntax(TermNode& term, _tIn first, _tIn last, _fTokenize tokenize)
 
 	tms.push(std::move(term));
 	for(; first != last; ++first)
-		if(*first == "(")
+		if(*first == "(" || *first == "[" || *first == "{")
+		{
 			tms.push(AsTermNode());
-		else if(*first != ")")
+			tms.top().Value = *first;
+		}
+		else if(!(*first == ")" || *first == "]" || *first == "}"))
 		{
 			assert(!tms.empty());
 			tms.top().Add(tokenize(*first));
@@ -63,6 +68,17 @@ ReduceSyntax(TermNode& term, _tIn first, _tIn last, _fTokenize tokenize)
 			auto tm(std::move(tms.top()));
 
 			tms.pop();
+
+			const auto rtok(*first);
+			const auto& ltok(tm.Value.GetObject<decltype(rtok)>());
+
+			if(rtok == ")" && ltok != "(")
+				ThrowMismatchBoundaryToken('(', ')');
+			if(rtok == "]" && ltok != "[")
+				ThrowMismatchBoundaryToken('[', ']');
+			if(rtok == "}" && ltok != "{")
+				ThrowMismatchBoundaryToken('{', '}');
+			tm.Value.Clear();
 			assert(!tms.empty());
 			tms.top().Add(std::move(tm));
 		}
@@ -70,7 +86,7 @@ ReduceSyntax(TermNode& term, _tIn first, _tIn last, _fTokenize tokenize)
 			return first;
 	if(tms.size() == 1)
 		return first;
-	throw UnilangException("Redundant '(' found.");
+	throw UnilangException("Redundant '(', '[' or '{' found.");
 }
 
 } // namespace Unilang;
