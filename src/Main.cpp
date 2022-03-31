054c5eda4156d14137eed144726ace041d715fe3
He Bin <hebin@uniontech.com>
2022-03-31 08:19:30 +0800

    Added function in module std.math: floor-remainder.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/Main.cpp
+++ src/Main.cpp
@@ -377,6 +377,8 @@ LoadModule_std_math(Interpreter& intp)
 	RegisterBinary<Strict, NumberNode, NumberNode>(ctx, "floor/", FloorDivides);
 	RegisterBinary<Strict, NumberNode, NumberNode>(ctx, "floor-quotient",
 		FloorQuotient);
+	RegisterBinary<Strict, NumberNode, NumberNode>(ctx, "floor-remainder",
+		FloorRemainder);
 	RegisterBinary<Strict, const int, const int>(ctx, "div",
 		[](const int& e1, const int& e2){
 		if(e2 != 0)
@@ -959,7 +961,7 @@ PrintHelpMessage(const string& prog)
 
 
 #define APP_NAME "Unilang interpreter"
-#define APP_VER "0.10.29"
+#define APP_VER "0.10.30"
 #define APP_PLATFORM "[C++11] + YSLib"
 constexpr auto
 	title(APP_NAME " " APP_VER " @ (" __DATE__ ", " __TIME__ ") " APP_PLATFORM);
