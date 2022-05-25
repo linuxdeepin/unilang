df29892c79aa67b79300ae5704b1694f70d15d10
He Bin <hebin@uniontech.com>
2022-05-25 14:45:48 +0800

    {include, src}/Parser: Enhanced class ByteParser and added class SourcedByteParser.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,61 +1,124 @@
-﻿// © 2020-2021 Uniontech Software Technology Co.,Ltd.
+﻿// © 2020-2022 Uniontech Software Technology Co.,Ltd.
 
 #include "Parser.h"
-#include "Lexical.h" // for IsDelimiter, IsGraphicalDelimiter;
 #include <cassert> // for assert;
+#include "Lexical.h" // for IsDelimiter, IsGraphicalDelimiter;
 
 namespace Unilang
 {
 
-void
-ByteParser::operator()(char c)
+namespace
 {
-	auto& lexer(GetLexerRef());
-	const auto add = [&](string&& arg){
-		lexemes.push_back(yforward(arg));
-	};
-	auto& cbuf(GetBufferRef());
-	const bool got_delim(lexer.FilterChar(c, GetBufferRef())
-		&& lexer.UpdateBack(GetBackRef(), c));
+
+template<typename _fAdd, typename _fAppend, class _tParseResult>
+void
+UpdateByteRaw(_fAdd add, _fAppend append, _tParseResult& res, bool got_delim,
+	const LexicalAnalyzer& lexer, string& cbuf, bool& update_current)
+{
 	const auto len(cbuf.length());
 
-	assert(!(lexemes.empty() && update_current) && "Invalid state found.");
+	assert(!(res.empty() && update_current) && "Invalid state found.");
 	if(len > 0 && !lexer.GetUnescapeContext().IsHandling())
 	{
 		if(len == 1)
 		{
-			const auto update_c([&](char b){
+			const auto update_c([&](char c){
 				if(update_current)
-					lexemes.back() += b;
+					append(res, c);
 				else
-					add(string({b}, lexemes.get_allocator()));
+					add(res, string({c}, res.get_allocator()));
 			});
-			const char b(cbuf.back());
+			const char c(cbuf.back());
 			const bool unquoted(lexer.GetDelimiter() == char());
 
- 			if(got_delim)
+			if(got_delim)
 			{
-				update_c(b);
+				update_c(c);
 				update_current = !unquoted;
 			}
-			else if(unquoted && IsDelimiter(b))
+			else if(unquoted && IsDelimiter(c))
 			{
-				if(IsGraphicalDelimiter(b))
-					add(string({b}, lexemes.get_allocator()));
+				if(IsGraphicalDelimiter(c))
+					add(res, string({c}, res.get_allocator()));
 				update_current = {};
 			}
 			else
 			{
-				update_c(b);
+				update_c(c);
 				update_current = true;
 			}
 		}
 		else if(update_current)
-			lexemes.back() += std::move(cbuf);
+			append(res, std::move(cbuf));
 		else
-			add(std::move(cbuf));
+			add(res, std::move(cbuf));
 		cbuf.clear();
 	}
+}
+
+template<class _tParseResult>
+struct SequenceAdd final
+{
+	template<typename _tParam>
+	void
+	operator()(_tParseResult& res, _tParam&& arg) const
+	{
+		res.push_back(yforward(arg));
+	}
+};
+
+template<class _tParseResult>
+struct SequenceAppend final
+{
+	template<typename _tParam>
+	void
+	operator()(_tParseResult& res, _tParam&& arg) const
+	{
+		res.back() += yforward(arg);
+	}
+};
+
+template<class _tParseResult>
+struct SourcedSequenceAdd final
+{
+	const SourcedByteParser& Parser;
+
+	template<typename _tParam>
+	void
+	operator()(_tParseResult& res, _tParam&& arg) const
+	{
+		res.emplace_back(Parser.GetSourceLocation(), yforward(arg));
+	}
+};
+
+template<class _tParseResult>
+struct SourcedSequenceAppend final
+{
+	template<typename _tParam>
+	void
+	operator()(_tParseResult& res, _tParam&& arg) const
+	{
+		res.back().second += yforward(arg);
+	}
+};
+
+} // unnamed namespace;
+
+
+void
+ByteParser::Update(bool got_delim)
+{
+	UpdateByteRaw(SequenceAdd<ParseResult>(), SequenceAppend<ParseResult>(),
+		lexemes, got_delim, GetLexerRef(), GetBufferRef(), update_current);
+}
+
+
+void
+SourcedByteParser::Update(bool got_delim)
+{
+	UpdateByteRaw(SourcedSequenceAdd<ParseResult>{*this},
+		SourcedSequenceAppend<ParseResult>(), lexemes, got_delim, GetLexerRef(),
+		GetBufferRef(), update_current);
 }
 
 } // namespace Unilang;
