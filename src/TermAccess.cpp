cd23be655242b5d7efaf0ed68a04ba42814b7772
He Bin <hebin@uniontech.com>
2022-07-27 13:50:22 +0800

    {include, src}/{TermAccess, Exception}: Moved exception throwing functions to the exception module.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/TermAccess.cpp
+++ src/TermAccess.cpp
@@ -37,56 +37,6 @@ TermToTags(TermNode& term)
 		TryAccessLeafAtom<const TermReference>(term), term.Tags);
 }
 
-void
-ThrowInsufficientTermsError(const TermNode& term, bool has_ref)
-{
-	throw ParameterMismatch(ystdex::sfmt(
-		"Insufficient subterms found in '%s' for the list parameter.",
-		TermToStringWithReferenceMark(term, has_ref).c_str()));
-}
-
-void
-ThrowListTypeErrorForInvalidType(const char* name, const TermNode& term,
-	bool has_ref)
-{
-	throw ListTypeError(ystdex::sfmt("Expected a value of type '%s', got a list"
-		" '%s'.", name, TermToStringWithReferenceMark(term, has_ref).c_str()));
-}
-void
-ThrowListTypeErrorForInvalidType(const type_info& ti,
-	const TermNode& term, bool has_ref)
-{
-	ThrowListTypeErrorForInvalidType(ti.name(), term, has_ref);
-}
-
-void
-ThrowListTypeErrorForNonlist(const TermNode& term, bool has_ref)
-{
-	throw ListTypeError(ystdex::sfmt("Expected a list, got '%s'.",
-		TermToStringWithReferenceMark(term, has_ref).c_str()));
-}
-
-void
-ThrowTypeErrorForInvalidType(const char* name, const TermNode& term,
-	bool has_ref)
-{
-	throw TypeError(ystdex::sfmt("Expected a value of type '%s', got '%s'.",
-		name, TermToStringWithReferenceMark(term, has_ref).c_str()));
-}
-void
-ThrowTypeErrorForInvalidType(const type_info& ti, const TermNode& term,
-	bool has_ref)
-{
-	ThrowTypeErrorForInvalidType(ti.name(), term, has_ref);
-}
-
-void
-ThrowValueCategoryError(const TermNode& term)
-{
-	throw ValueCategoryMismatch(ystdex::sfmt("Expected a reference for the 1st "
-		"argument, got '%s'.", TermToString(term).c_str()));
-}
-
 
 #if Unilang_CheckTermReferenceIndirection
 TermNode&
