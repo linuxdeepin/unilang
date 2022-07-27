6e556a9e939655b2bd32b0d754db0f5cc59abbd6
He Bin <hebin@uniontech.com>
2022-07-27 15:10:33 +0800

    {include, src}/Exception: {ThrowInsufficientTermsError, ThrowListTypeErrorForInvalidType, ThrowListTypeErrorForNonlist, ThrowTypeErrorForInvalidType}: Supported printing improper lists and skipping the subterm prefix.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- include/Exception.h
+++ include/Exception.h
@@ -139,20 +139,23 @@ public:
 
 
 YB_NORETURN void
-ThrowInsufficientTermsError(const TermNode&, bool);
+ThrowInsufficientTermsError(const TermNode&, bool, size_t = 0);
 
 YB_NORETURN YB_NONNULL(1) void
-ThrowListTypeErrorForInvalidType(const char*, const TermNode&, bool);
+ThrowListTypeErrorForInvalidType(const char*, const TermNode&, bool,
+	size_t = 0);
 YB_NORETURN void
-ThrowListTypeErrorForInvalidType(const type_info&, const TermNode&, bool);
+ThrowListTypeErrorForInvalidType(const type_info&, const TermNode&, bool,
+	size_t = 0);
 
 YB_NORETURN void
-ThrowListTypeErrorForNonlist(const TermNode&, bool);
+ThrowListTypeErrorForNonlist(const TermNode&, bool, size_t = 0);
 
 YB_NORETURN YB_NONNULL(1) void
-ThrowTypeErrorForInvalidType(const char*, const TermNode&, bool);
+ThrowTypeErrorForInvalidType(const char*, const TermNode&, bool, size_t = 0);
 YB_NORETURN void
-ThrowTypeErrorForInvalidType(const type_info&, const TermNode&, bool);
+ThrowTypeErrorForInvalidType(const type_info&, const TermNode&, bool,
+	size_t = 0);
 
 YB_NORETURN void
 ThrowValueCategoryError(const TermNode&);
