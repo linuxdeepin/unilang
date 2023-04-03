b26026c87d2a4d3eccb902613ce3e28f65b3de6b
He Bin <hebin@uniontech.com>
2023-04-03 13:43:29 +0800

    src/Evaluation.cpp: Fixed unexpected copy of 'ValueObject' initialized on checking the type of 'Reducer' for lightweight continuations.
    This was broken since commit 065b1c31793a30d320a7a95c8943f829c5da71e7.
    
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/Evaluation.cpp
+++ src/Evaluation.cpp
@@ -1632,7 +1632,7 @@ AddTypeNameTableEntry(const type_info& ti, string_view sv)
 string_view
 QueryContinuationName(const Reducer& act)
 {
-	if(IsTyped<GLContinuation<>>(act))
+	if(IsTyped<GLContinuation<>>(act.target_type()))
 		return QueryTypeName(type_id<ContextHandler>());
 	if(const auto p_cont = act.target<Continuation>())
 		return QueryTypeName(p_cont->Handler.target_type());
