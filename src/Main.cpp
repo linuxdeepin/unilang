05002e19c2f724792d563a6ec0c01ce4c291a316
He Bin <hebin@uniontech.com>
2023-05-23 13:24:19 +0800

    Added function: $import&!.
    Signed-off-by: He Bin <hebin@uniontech.com>

--- src/Main.cpp
+++ src/Main.cpp
@@ -1000,6 +1000,9 @@ $defv! $provide! (&symbols .&body) d
 $defv! $import! (&e .&symbols) d
 	eval% (list $set! d (append symbols ((unwrap list%) .))
 		(symbols->imports symbols)) (eval e d);
+$defv! $import&! (&e .&symbols) d
+	eval% (list $set! d (append (map1 ensigil symbols)
+		((unwrap list%) .)) (symbols->imports symbols)) (eval e d);
 	)Unilang");
 	LoadStandardDerived(intp);
 	// NOTE: Supplementary functions.
@@ -1158,7 +1161,7 @@ PrintHelpMessage(const string& prog)
 
 
 #define APP_NAME "Unilang interpreter"
-#define APP_VER "0.12.343"
+#define APP_VER "0.12.344"
 #define APP_PLATFORM "[C++11] + YSLib"
 constexpr auto
 	title(APP_NAME " " APP_VER " @ (" __DATE__ ", " __TIME__ ") " APP_PLATFORM);
